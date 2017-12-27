ec80c6548bce650204658d3e374d1430c59478ea
gks-spa <gks-spa@users.noreply.github.com>
2017-12-26 18:27:54 -0600

    Add TestClass

---
+++ src/main/java/edu/spa/ftclib/sample/TestClass.java
@@ -0,0 +1,12 @@
+package edu.spa.ftclib.sample;
+
+/**
+ * Created by Gabriel on 2017-12-26.
+ * Use this to test if you've installed the library correctly.
+ */
+
+public class TestClass {
+    public final String test() {
+        return "test";
+    }
+}
