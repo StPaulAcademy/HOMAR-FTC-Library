67e31bb5d13d886bf1a1836ba293427578b45b21
19michaelaap <19michaelaap@students.spa.edu>
2017-12-28 16:56:22 -0600

    Putting in Javadoc comments for the sample and toggle packages

--- src/main/java/edu/spa/ftclib/toggle/ToggleBoolean.java
+++ src/main/java/edu/spa/ftclib/toggle/ToggleBoolean.java
@@ -8,16 +8,36 @@ package edu.spa.ftclib.toggle;
 public class ToggleBoolean {
     private boolean toggle = false; //Current state of the toggle
     private boolean previouslyActuated = false; //Was the actuator (whatever is passed to input) true last time?
-    public void input(boolean currentlyActuated) {  //Call this method in your main op-mode loop; pass it the state of whatever actuator (eg. button) you want to use to control the toggle
+
+    /**
+     * Call this method in your main op-mode loop
+     * @param currentlyActuated the state of the actuater you want to use to control the toggle
+     */
+    public void input(boolean currentlyActuated) {
+        //if the button is currently being pressed and and wasn't being pressed the last time through the loop, change the toggle
         if (currentlyActuated && !previouslyActuated) toggle();
-        previouslyActuated = currentlyActuated;
+        previouslyActuated = currentlyActuated; //change previouslyActuated to reflect the current state of the toggle
     }
+
+    /**
+     * Changes the value of the toggle variable to false if it was true and true if it was false
+     */
     public void toggle() {  //Toggle the toggle (generally you shouldn't need this)
         toggle = !toggle;
     }
+
+    /**
+     *
+     * @param toggle holds the current state of the toggle
+     */
     public void setToggle(boolean toggle) { //Set the toggle to a specific value (generally you shouldn't need this)
         this.toggle = toggle;
     }
+
+    /**
+     *
+     * @return the current state of the toggle
+     */
     public boolean output() {   //Get the value of the toggle; call this whenever you need to use the toggle to control something
         return toggle;
     }
--- src/main/java/edu/spa/ftclib/toggle/ToggleInt.java
+++ src/main/java/edu/spa/ftclib/toggle/ToggleInt.java
@@ -3,26 +3,54 @@ package edu.spa.ftclib.toggle;
 /**
  * Created by Gabriel on 10/31/2017.
  * Use to implement a 0, 1, 2, ..., (maxToggle-1), 0, 1, 2, ... toggle that toggles (increments or revert to 0) when a button is pressed
+ * An example of when you might want to use this is when you have multiple motors that you might want the same button to control.
+ * Use a switch in your code and for each value of the toggle, set a different motor to be used.
  */
 
 public class ToggleInt {
     private int toggle = 0; //Current state of the toggle
     private boolean previouslyActuated = false; //Was the actuator (whatever is passed to input) true last time?
-    private final int maxToggle;
+    private final int maxToggle; //the number of states that you want to toggle through
+
+    /**
+     *
+     * @param maxToggle the number of states that you want to toggle through
+     */
     public ToggleInt(int maxToggle) {   //Pass this the number of values you want the toggle to have. When the toggle is about to reach this number, it will go back to 0.
         this.maxToggle = maxToggle;
     }
-    public void input(boolean currentlyActuated) {  //Call this method in your main op-mode loop; pass it the state of whatever actuator (eg. button) you want to use to control the toggle
+
+    /**
+     * Call this method in your main op-mode loop
+     * @param currentlyActuated the state of whatever actuator (eg. button) you want to use to control the toggle
+     */
+    public void input(boolean currentlyActuated) {
+        //if the button is being pressed now and wasn't being pressed last time, change the value of the toggle
         if (currentlyActuated && !previouslyActuated) toggle();
+        //change previoulyActuated to reflect the current state of the toggle
         previouslyActuated = currentlyActuated;
     }
-    public void toggle() {  //Toggle the toggle (generally you shouldn't need this)
+
+    /**
+     * Where the actual toggling happens. Generally you shouldn't need this
+     */
+    public void toggle() {
         toggle++;
         toggle %= maxToggle;
     }
+
+    /**
+     *
+     * @param toggle the current state of the toggle
+     */
     public void setToggle(int toggle) { //Set the toggle to a specific value (generally you shouldn't need this)
         this.toggle = toggle;
     }
+
+    /**
+     *
+     * @return the current state of the toggle
+     */
     public int output() {   //Get the value of the toggle; call this whenever you need to use the toggle to control something
         return toggle;
     }
