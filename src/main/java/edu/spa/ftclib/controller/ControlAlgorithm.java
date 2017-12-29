62367c0a1603aa2a0547680bc484eb45423b02d2
19michaelaap <19michaelaap@students.spa.edu>
2017-12-28 19:36:12 -0600

    Putting in Javadoc comments for the controller package

--- src/main/java/edu/spa/ftclib/controller/ControlAlgorithm.java
+++ src/main/java/edu/spa/ftclib/controller/ControlAlgorithm.java
@@ -2,8 +2,14 @@ package edu.spa.ftclib.controller;
 
 /**
  * Created by Gabriel on 2017-12-28.
+ * The fancy math that gives the system a usable output.
+ * @see PIDController
  */
 
 public interface ControlAlgorithm extends Controller {
+    /**
+     * Give the control algorithm an input, which allows it to do fancy math and give an output.
+     * @param input The input into the fancy math, such as the heading from a gyro.
+     */
     void input(double input);
 }
--- src/main/java/edu/spa/ftclib/controller/SensorController.java
+++ src/main/java/edu/spa/ftclib/controller/SensorController.java
@@ -2,26 +2,58 @@ package edu.spa.ftclib.controller;
 
 /**
  * Created by Gabriel on 2017-12-28.
+ * An updateable controller which uses a sensor to get its inputs.
  */
 
 public abstract class SensorController implements UpdateableController {
+    /**
+     * Chooses which control algorithm to use.
+     * @see PIDController
+     */
     ControlAlgorithm algorithm;
+
+    /**
+     * Sets the algorithm being used to be the algorithm specified by the user.
+     * @param algorithm Which control algorithm should be used
+     */
     public SensorController(ControlAlgorithm algorithm) {
         this.algorithm = algorithm;
     }
+
+    /**
+     * Gets the value from the sensor to be used as an input.
+     * @return The input value from the sensor
+     */
     protected abstract double getSensorValue();
+
+    /**
+     * Gives the sensor value to the algorithm so that the algorithm can calculate a usable value for the system.
+     */
     public void update() {
         algorithm.input(getSensorValue());
     }
+
+    /**
+     * Returns the value from the algorithm for use by the system.
+     * @return The final calculation from the algorithm
+     */
     public double output() {
         return algorithm.output();
     }
 
+    /**
+     * Recieves a target value from the user.
+     * @param target The value the system is trying to reach or maintain
+     */
     @Override
     public void setTarget(double target) {
         algorithm.setTarget(target);
     }
 
+    /**
+     * Gets the target value the system is currently trying to reach or maintain.
+     * @return The current target value
+     */
     @Override
     public double getTarget() {
         return algorithm.getTarget();
