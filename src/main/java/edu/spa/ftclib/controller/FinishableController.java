a38c6c2c7f6ea27f1f80255b8b81b12da606d183
19michaelaap <19michaelaap@students.spa.edu>
2017-12-29 13:49:37 -0600

    Putting in Javadoc comments for the controller and drivetrain packages

--- src/main/java/edu/spa/ftclib/controller/FinishableController.java
+++ src/main/java/edu/spa/ftclib/controller/FinishableController.java
@@ -2,8 +2,13 @@ package edu.spa.ftclib.controller;
 
 /**
  * Created by Gabriel on 2017-12-29.
+ * A controller which is used when the action has an endpoint, such as making a turn or driving a specific distance.
  */
 
 public interface FinishableController extends Controller {
+    /**
+     * Determines if the action is complete.
+     * @return If the system is done
+     */
     boolean finished();
 }
