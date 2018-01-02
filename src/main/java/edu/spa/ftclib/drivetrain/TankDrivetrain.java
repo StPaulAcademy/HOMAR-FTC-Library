a823751af9d679c861871e78350f01874609135b
19michaelaap <19michaelaap@students.spa.edu>
2018-01-02 16:57:31 -0600

    Adding Positionable Interface to TankDrivetrain.

--- src/main/java/edu/spa/ftclib/drivetrain/TankDrivetrain.java
+++ src/main/java/edu/spa/ftclib/drivetrain/TankDrivetrain.java
@@ -7,8 +7,14 @@ import com.qualcomm.robotcore.hardware.DcMotor;
  * A simple drivetrain with two drive wheels.
  */
 
-public class TankDrivetrain extends Drivetrain implements Rotatable {
+public class TankDrivetrain extends Drivetrain implements Rotatable, Positionable {
     private double rotation = 0;
+
+    private double targetPosition = 0;
+
+    private DcMotor.RunMode[] runModes = new DcMotor.RunMode[2];
+
+    private double[] wheelTargetPositions = new double[2];
 
     /**
      * The constructor for the class.
@@ -58,5 +64,66 @@ public class TankDrivetrain extends Drivetrain implements Rotatable {
         motorPowers[0] = getVelocity()-rotation; //calculate the motor power for the left wheel
         motorPowers[1] = getVelocity()+rotation; //calculate the motor power for the right wheel
         return motorPowers;
+    }
+
+    /**
+     * @param position the position that you want the robot to move to
+     */
+    @Override
+    public void setTargetPosition(double position) {
+        for (int i = 0; i < runModes.length; i++) {
+            runModes[i] = motors[i].getMode();  //Save the RunModes so we can restore them later
+        }
+        this.targetPosition = position;
+        for (DcMotor motor : motors) motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        for (DcMotor motor : motors) motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        for (int i = 0; i < motors.length; i++) {   //Calculate how far each wheel has to go to get the drivetrain to a specific position
+            wheelTargetPositions[0] = targetPosition*(1-rotation);
+            wheelTargetPositions[1] = targetPosition * (1+rotation);
+            motors[i].setTargetPosition((int)(wheelTargetPositions[i]+0.5));    //Round to the nearest int because setTargetPosition only accepts ints
+        }
+        updateMotorPowers();
+    }
+
+    /**
+     * @return the current position of the robot
+     */
+    @Override
+    public double getCurrentPosition() {
+        return (motors[0].getCurrentPosition() + motors[1].getCurrentPosition())/2;
+    }
+
+    /**
+     * @return the position the robot is supposed to move to (the Target Position)
+     */
+    @Override
+    public double getTargetPosition() {
+        return targetPosition;
+    }
+
+    /**
+     * Recalculate motor powers to maintain or move towards the target position
+     */
+    @Override
+    public void updatePosition() {
+    }
+
+    /**
+     * Use this as a loop condition (with {@link #updatePosition in the loop body) if you want to move to a specific position and then move on to other code.
+     *
+     * @return Whether or not the drivetrain is still moving towards the target position
+     */
+    @Override
+    public boolean isPositioning() {
+        if (motors[0].isBusy() || motors[1].isBusy()) return true;
+        else return false;
+    }
+
+    /**
+     * If there are any settings (motor {@link DcMotor.RunMode RunModes}, etc.) you changed to position, change them back in this method.
+     */
+    @Override
+    public void finishPositioning() {
+
     }
 }
