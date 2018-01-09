656724f3538f0a6f7a140819f2c46c11a101f178
gks-spa <gks-spa@users.noreply.github.com>
2018-01-09 09:51:27 -0600

    Add test op-mode

---
+++ src/main/java/edu/spa/ftclib/sample/opmode/TestOpMode.java
@@ -0,0 +1,31 @@
+package edu.spa.ftclib.sample.opmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import edu.spa.ftclib.sample.TestClass;
+
+/**
+ * Created by Gabriel on 2018-01-09.
+ */
+
+public class TestOpMode extends OpMode {
+    /**
+     * User defined init method
+     * <p>
+     * This method will be called once when the INIT button is pressed.
+     */
+    @Override
+    public void init() {
+        telemetry.addData("test in init", TestClass.test());
+    }
+
+    /**
+     * User defined loop method
+     * <p>
+     * This method will be called repeatedly in a loop while this op mode is running
+     */
+    @Override
+    public void loop() {
+        telemetry.addData("test in loop", TestClass.test());
+    }
+}
